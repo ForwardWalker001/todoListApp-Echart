d57313331465d8c82a7d300fab35df4608c4676b
rookie <49440935+ForwardWalker001@users.noreply.github.com>
2022-01-14 11:04:05 +0800

    修改首次进入应用的情况

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    todos:JSON.parse(localStorage.getItem('todoData') || [])
+    todos:JSON.parse(localStorage.getItem('todoData')) || []
   },
   getters:{
     remaining: function (state) {
